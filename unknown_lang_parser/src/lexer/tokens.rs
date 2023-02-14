a6467a3c51a007569c55eaa0293c546aad0bfd81
Braxton Fair <hello@taxborn.com>
2023-02-13 22:38:13 -0600

    Start tokens list

---
+++ unknown_lang_parser/src/lexer/tokens.rs
@@ -0,0 +1,48 @@
+//! Tokens for unknown-lang parser
+pub enum Tokens {
+    /// (
+    LPar,
+    /// )
+    RPar,
+    /// [
+    LBracket,
+    RBracket,
+    LBrace,
+    RBrace,
+    Eq,
+    EqEq,
+    Colon,
+    ColonColon,
+    Semi,
+    FatArrow,
+    Dollar,
+    Comma,
+    RightArrow,
+    Dot,
+    DotDot,
+    Tilde,
+
+    Char(char),
+    Str(String),
+    // TODO: Numbers
+
+    // Operators
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    Ampersand,
+    Bar,
+    Hat,
+    Greater,
+    GreaterEq,
+    GreaterGreater,
+    Less,
+    LessEq,
+    LessLess,
+    Bang,
+    BangEq,
+
+    PlusEq,
+}
